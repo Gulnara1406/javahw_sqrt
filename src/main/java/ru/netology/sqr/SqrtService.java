69dea0a5eba62eb6619e7eae8f717674991e8996
Name Gulnara <nabgulnara@mail.ru>
2023-04-06 19:22:57 +0300

    project created

---
+++ src/main/java/ru/netology/sqr/SqrtService.java
@@ -0,0 +1,14 @@
+package ru.netology.sqr;
+
+public class SqrtService {
+
+    public int calcSqrt(int a, int x){
+        int k=0; //счетчик кол-ва чисел
+        for (int i = 10; i <=99; i++){
+            if (Math.pow(i,2) >= a && Math.pow(i,2) <= x) {
+                k+=1;
+            }
+        }
+        return k;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SqrtServiceTest.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class SqrtServiceTest {
+    @ParameterizedTest()
+    @CsvFileSource(files = "src/test/resources/sqrt.csv")
+    public void testInLimit(int expected, int a, int x) {
+        SqrtService service = new SqrtService();
+        int actual = service.calcSqrt(a, x);
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
